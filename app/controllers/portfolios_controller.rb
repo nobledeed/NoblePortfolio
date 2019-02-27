2333660b2e25d8d16e8ae73f63342588378b704a
Dean Noble <nobled@spu.edu>
2019-02-27 09:51:58 -0800

    Intgrated new and create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,9 +1,22 @@
 class PortfoliosController < ApplicationController
-def index
-    @portfolio_items =Portfolio.all
-end
+    def index
+        @portfolio_items =Portfolio.all
+    end
 
-
-
-
+    def new
+        @portfolio_item = Portfolio.new
+    end
+    def create
+        @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    
+        respond_to do |format|
+          if @portfolio_item.save
+            format.html { redirect_to portfolios_path, notice: 'Portfolio item is now live.' }
+            format.json { render :show, status: :created, location: @blog }
+          else
+            format.html { render :new }
+            format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+          end
+        end
+      end
 end
