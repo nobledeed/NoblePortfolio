a974ade52a93861c11eff748fd8eefb386a56a9f
Dean Noble <nobled@spu.edu>
2019-02-27 12:45:51 -0800

    Added new and edit buttons for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -16,7 +16,23 @@ class PortfoliosController < ApplicationController
           else
             format.html { render :new }
             format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+           end
+        end
+    end
+    def edit
+            @portfolio_item = Portfolio.find(params[:id])
+    end
+    def update
+        @portfolio_item = Portfolio.find(params[:id])
+        respond_to do |format|
+          if @portfolio_item.update (params.require(:portfolio).permit(:title, :subtitle, :body))
+            format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+            format.json { render :show, status: :ok, location: @blog }
+          else
+            format.html { render :edit }
+            format.json { render json: @blog.errors, status: :unprocessable_entity }
           end
         end
-      end
+    end
+
 end
