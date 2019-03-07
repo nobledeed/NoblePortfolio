115d464faa7da961eb3843ae9153f1e8df1360b2
Dean Noble <nobled@spu.edu>
2019-03-07 11:44:29 -0800

    Implemented deaulft values for portfolio images

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,6 +2,17 @@ class Portfolio < ApplicationRecord
     validates_presence_of :title, :body, :main_image, :thumb_image
     def self.rails
         where(subtitle: 'Ruby on Rails')
-        end
-scope :codeigniter, -> { where(subtitle: 'Code Igniter')}
+        
+    end
+    
+    scope :codeigniter, -> { where(subtitle: 'Code Igniter')}
+
+    after_initialize :set_defaults
+
+
+    def set_defaults
+        self.main_image ||= "https://place-hold.it/600x400" 
+        self.thumb_image ||= "https://place-hold.it/350x150"
+
+    end
 end
