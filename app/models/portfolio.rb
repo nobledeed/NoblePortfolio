c11345dce2646eb6f66df67be51c00f9e2a69fd1
Dean Noble <nobled@spu.edu>
2019-03-08 07:32:36 -0800

    Created Placeholder concern

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,18 +1,19 @@
 class Portfolio < ApplicationRecord
+    include Placeholder
     validates_presence_of :title, :body, :main_image, :thumb_image
     def self.rails
         where(subtitle: 'Ruby on Rails')
         
     end
-    
+
     scope :codeigniter, -> { where(subtitle: 'Code Igniter')}
 
     after_initialize :set_defaults
 
 
     def set_defaults
-        self.main_image ||= "https://place-hold.it/600x400" 
-        self.thumb_image ||= "https://place-hold.it/350x150"
+        self.main_image ||= Placeholder.image_generator(height: '600',width: '400') 
+        self.thumb_image ||= Placeholder.image_generator(height: '350',width: '150') 
 
     end
 end
